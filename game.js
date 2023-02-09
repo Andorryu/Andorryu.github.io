190047951764942e79bcc66d264ebdf9c4cda5d0
Andorryu <adority121@gmail.com>
2023-02-08 23:12:32 -0600

    color change on click

--- game.js
+++ game.js
@@ -30,24 +30,31 @@ function checkWin(board) {
     return "no winner";
 }
 
+function onclick() {
+    this.style.fill = "yellow"; // 'this' refers to the space, since it is called in space's method
+    console.log("click!")
+    this.removeEventListener("click", onclick);
+
+    let pos = [
+        space.getAttribute("row"),
+        space.getAttribute("col")
+    ]; // row, col
+
+    // update board with change
+    board[pos[0]][pos[1]] = new Symbol("p1");
+    space.removeEventListener("click", onclick)
+    // update UI
+    for (row of board) {
+        for (space of row) {
+            
+        }
+    }
+}
+
+
+
 // add event listeners for clicking all spaces
 for (space of document.querySelectorAll(".spaces")) {
-    space = Element(space)
-    space.addEventListener("click", () => {
-        let pos = [
-            space.getAttribute("row"),
-            space.getAttribute("col")
-        ]; // row, col
-
-        // update board with change
-        board[pos[0]][pos[1]] = new Symbol("x");
-
-        // update UI
-        for (row of board) {
-            for (space of row) {
-                
-            }
-        }
-    });
+    space.addEventListener("click", onclick);
 }
 
